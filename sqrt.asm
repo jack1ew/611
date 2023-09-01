dcfd34ca79ac988f544506f7194c8718d59f3821
jack1ew <jackiew@email.sc.edu>
2023-09-01 16:59:49 -0400

    First commit

--- sqrt.asm
+++ sqrt.asm
@@ -1,1 +1,8 @@
+# Inital goal would be 0 and the initial step will be 256
+# Input is already prescaled by 2^14
+# The condition given for input_actual and output_actual is not done through RISC-V but by hand.
+
+# Read int
+    li a7, 5
+    ecall
 
