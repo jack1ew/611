09db6259337c1559fa52a6baa92ffd18edb07b75
jack1ew <112125667+jack1ew@users.noreply.github.com>
2023-09-04 13:37:13 -0400

    Update sqrt.asm

--- sqrt.asm
+++ sqrt.asm
@@ -1,6 +1,7 @@
 # Inital goal would be 0 and the initial step will be 256
 # Input is already prescaled by 2^14
 # The condition given for input_actual and output_actual is not done through RISC-V but by hand.
+# Do until the steps are 0
 
 # Read int
     li a7, 5
