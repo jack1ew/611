cf6aef579919d63463da708eb7d0295acdca6ed7
jack1ew <112125667+jack1ew@users.noreply.github.com>
2023-10-11 18:04:43 -0400

    Update decode.sv

--- lab_riu/decode.sv
+++ lab_riu/decode.sv
@@ -2,38 +2,27 @@ module decode (
 
 /**** inputs *****************************************************************/
 
-	input [31:0] instructaddr,
+	input logic [31:0] instructaddr,
 	
 /**** outputs ****************************************************************/
 
-	output [6:0] funct7,
-	output [4:0] rs2,
-	output [4:0] rs1,
-	output [2:0] fuct3.
-	output [4:0] rd,
-	output [6:0] opcode,
-	output [20:0] imm20,
-	output [12:0] imm12,
+	output logic [6:0] funct7,
+	output logic [4:0] rs2,
+	output logic [4:0] rs1,
+	output logic [2:0] funct3,
+	output logic [4:0] rd,
+	output logic [6:0] opcode,
+	output logic [19:0] imm20,
+	output logic [11:0] imm12
 );
-	always_comb begin
-		if (opcode == 7'b0110011) begin
-			funct7 = instructaddr [31:25];
-			rs2 = instructaddr [24:20];
-			rs1 = instructadddr [19:15];
-			funct3 = instructaddr [14:12];
-			rd = instructadder [11:7];
-			opcode = instructadder [6:0];
-		end else if (opcode == 7'b0010011) begin
-			imm12 = instructadder [31:20];//
-			rs1 = instructadder [19:15];
-			funct3 = instructadder [14:12];
-			rd = instructadder [11:7];
-			opcode = instructadder [6:0];
-		end else if (opcode = 7'b0110111) begin
-			imm20 = instuctadder [31:12];
-			rd = instructadder [11:7];
-			opcode = instructadder[6:0];
-		end
-	end
+        assign funct7 = instructaddr[31:25];
+        assign rs2 = instructaddr [24:20];
+        assign rs1 = instructaddr [19:15];
+        assign funct3 = instructaddr [14:12];
+        assign rd = instructaddr [11:7];
+        assign opcode = instructaddr [6:0];
+        assign imm20 = instructaddr [31:12];
+        assign imm12 = instructaddr [31:20];
+        
 endmodule
 		
