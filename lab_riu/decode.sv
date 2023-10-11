4902c25b506b79d5aecadfa1f3c4e9eb93b20fd7
jack1ew <112125667+jack1ew@users.noreply.github.com>
2023-10-11 12:28:09 -0400

    Update decode.sv

--- lab_riu/decode.sv
+++ lab_riu/decode.sv
@@ -15,25 +15,24 @@ module decode (
 	output [20:0] imm20,
 	output [12:0] imm12,
 );
-	always @ (*) begin
-		if (opcode == 7'b0110011)
+	always_comb begin
+		if (opcode == 7'b0110011) begin
 			funct7 = instructaddr [31:25];
 			rs2 = instructaddr [24:20];
 			rs1 = instructadddr [19:15];
 			funct3 = instructaddr [14:12];
 			rd = instructadder [11:7];
 			opcode = instructadder [6:0];
-		else begin
-			if (opcode == 7'b0010011)
-				imm12 = instructadder [31:20];//
-				rs1 = instructadder [19:15];
-				funct3 = instructadder [14:12];
-				rd = instructadder [11:7];
-				opcode = instructadder [6:0];
-			else if (opcode = 7'b0110111)
-				imm20 = instuctadder [31:12];
-				rd = instructadder [11:7];
-				opcode = instructadder[6:0];
+		end else if (opcode == 7'b0010011) begin
+			imm12 = instructadder [31:20];//
+			rs1 = instructadder [19:15];
+			funct3 = instructadder [14:12];
+			rd = instructadder [11:7];
+			opcode = instructadder [6:0];
+		end else if (opcode = 7'b0110111) begin
+			imm20 = instuctadder [31:12];
+			rd = instructadder [11:7];
+			opcode = instructadder[6:0];
 		end
 	end
 endmodule
