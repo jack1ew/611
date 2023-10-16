e6d1a6f049bfb4083aefd685f34dd0deda82844e
jack1ew <112125667+jack1ew@users.noreply.github.com>
2023-10-16 15:42:19 -0400

    Update controlunit.sv

--- lab_riu/controlunit.sv
+++ lab_riu/controlunit.sv
@@ -5,21 +5,20 @@ module controlunit (
 	input logic [2:0] funct3,
 	input logic [4:0] rd,
 	input logic [6:0] opcode,
-	input logic [20:0] imm20,
+	input logic [19:0] imm20,
 	input logic [11:0] imm12,
 	
 	output logic alusrc,
 	output logic regwrite,
-	output logic [1:0] regsel,
+	output logic [2:0] regsel,
 	output logic [3:0] op,
-	output logic gpio_we,
-	output logic [31:0] gpio_out
+	output logic gpio_we
 );
-	always @(*) begin
+	always@(funct3 or funct7 or opcode) begin
 		if (opcode == 7'b0010011 && funct3 == 3'b000 && funct7 == 7'b0000000) begin // addi
 			alusrc = 1'b1;
 			regwrite = 1'b1;
-			regsel = 2'b01;
+			regsel = 2'b10;
 			op = 4'b0011;
 			gpio_we = 1'b0;
 		end else if (opcode == 7'b0110011 && funct3 == 3'b000 && funct7 == 7'b0000000) begin // add
@@ -62,13 +61,13 @@ module controlunit (
 			alusrc = 1'b0;
 			regwrite = 1'b1;
 			regsel = 2'b10;
-			op = 4'b1001;
+			op = 4'b1011;
 			gpio_we = 1'b0;
 		end else if (opcode == 7'b0110011 && funct3 == 3'b101 && funct7 == 7'b0000000) begin // srl
 			alusrc = 1'b0;
 			regwrite = 1'b1;
 			regsel = 2'b10;
-			op = 4'b1011;
+			op = 4'b1001;
 			gpio_we = 1'b0;
 		end else if (opcode == 7'b0110011 && funct3 == 3'b010 && funct7 == 7'b0000000) begin // slt
 			alusrc = 1'b0;
@@ -100,17 +99,17 @@ module controlunit (
 			regsel = 2'b10;
 			op = 4'b0111;
 			gpio_we = 1'b0;
-		end else if (opcode == 7'b1110011 && funct3 == 3'b001 && imm12 == 12'b1111_0000_0010) begin // csrrw hex
-			alusrc = 1'bX;
-			regwrite = 1'b0;
-			regsel = 4'bX;
+		end else if (opcode == 7'b1110011 && funct3 == 3'b001 && imm12 == 12'hf02) begin // csrrw hex
 			op = 4'bX;
+			alusrc = 1'bX;
+			regsel = 2'bX;
+			regwrite = 1'b0;
 			gpio_we = 1'b1;
-		end else if (opcode == 7'b1110011 && funct3 == 3'b001 && imm12 == 12'b1111_0000_0000) begin // csrrw sw
+		end else if (opcode == 7'b1110011 && funct3 == 3'b001 && imm12 == 12'hf00) begin // csrrw sw
+			op = 4'bX;
 			alusrc = 1'bX;
 			regwrite = 1'b1;
 			regsel = 2'b00;
-			op = 4'bX;
 			gpio_we = 1'b0;
 		end else if (opcode == 7'b0010011 && funct3 == 3'b111) begin // andi
 			alusrc = 1'b1;
@@ -130,19 +129,19 @@ module controlunit (
 			regsel = 2'b10;
 			op = 4'b0010;
 			gpio_we = 1'b0;
-		end else if (opcode == 7'b0010011 && funct3 == 3'b001) begin // slli
+		end else if (opcode == 7'b0010011 && funct3 == 3'b001 && imm12[11:5] == 7'b0) begin // slli
 			alusrc = 1'b1;
 			regwrite = 1'b1;
 			regsel = 2'b10;
 			op = 4'b1000;
 			gpio_we = 1'b0;
-		end else if (opcode == 7'b0010011 && funct3 == 3'b101 && imm20 == 7'b0100000) begin // srai
+		end else if (opcode == 7'b0010011 && funct3 == 3'b101 && imm12[11:5] == 7'b0) begin // srai
 			alusrc = 1'b1;
 			regwrite = 1'b1;
 			regsel = 2'b10;
-			op = 4'b1001;
+			op = 4'b1011;
 			gpio_we = 1'b0;
-		end else if (opcode == 7'b0010011 && funct3 == 3'b101 && imm20 == 7'b0000000) begin // srli
+		end else if (opcode == 7'b0010011 && funct3 == 3'b101 && imm12[11:5] == 7'b0) begin // srli
 			alusrc = 1'b1;
 			regwrite = 1'b1;
 			regsel = 2'b10;
