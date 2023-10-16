6eecb8406587b74445f5fd366ec60aa882a05010
jack1ew <112125667+jack1ew@users.noreply.github.com>
2023-10-16 15:40:31 -0400

    Update top.sv

--- lab_riu/top.sv
+++ lab_riu/top.sv
@@ -92,14 +92,18 @@ module top (
 			leds <= leds >> 1;
 		end
 	end
+	
+	// HEX output
 	logic [31:0] hexout;
-	cpu cpu_(.clck(CLOCK_50), .rst_n(KEY[0]), .io0_n(SW), .io2_out(hexout));
+	logic [31:0] hexin;
+	assign hexin = {14'b0,SW};
+	cpu cpu_ (.clck(CLOCK_50), .rst_n(KEY[0]), .in(hexin), .out(hexout));
 	hexdriver d0 (hexout[3:0], HEX0);
 	hexdriver d1 (hexout[7:4], HEX1);
 	hexdriver d2 (hexout[11:8], HEX2);
 	hexdriver d3 (hexout[15:12], HEX3);
-	hexdriver d4 ({2'b00, hexout[17:16]}, HEX4);
-	hexdriver d5 (4'b0000, HEX5);
-	hexdriver d6 (4'b0000, HEX6);
-	hexdriver d7 (4'b0000, HEX7);
+	hexdriver d4 (hexout[19:16], HEX4);
+	hexdriver d5 (hexout[23:20], HEX5);
+	hexdriver d6 (hexout[27:24], HEX6);
+	hexdriver d7 (hexout[31:28], HEX7);
 endmodule
